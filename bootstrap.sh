88cebd60d08b492d7e05f8d1e2b47d9e516003fe
z <z@z>
2020-07-05 02:24:34 +0800

    fix cp and avoid source twice

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-cp -r ~/.local/etc .
-if [[ $dddddudsaf = 1 ]] {
+#!/usr/bin/zsh
+# cover local config
+cp -r etc ~/.local/
+# enable config file and avoid source twice
+if [[ $owen_inited = 1 ]] {
 	echo "source ~/.local/etc/zsh.conf">>~/.zshrc
 	echo "source ~/.local/etc/tmux.conf">>~/.tmux.conf
 }
