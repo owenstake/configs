47f19b8563a45b017daea190332ed62b1e52c7a8
z <z@z>
2020-07-05 01:55:57 +0800

    cp config to .local

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,6 @@
-echo "source ~/.local/etc/zsh.conf">>~/.zshrc
-echo "source ~/.local/etc/tmux.conf">>~/.tmux.conf
+#!/bin/bash
+cp -r ~/.local/etc .
+if [[ $dddddudsaf = 1 ]] {
+	echo "source ~/.local/etc/zsh.conf">>~/.zshrc
+	echo "source ~/.local/etc/tmux.conf">>~/.tmux.conf
+}
