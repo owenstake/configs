b89ccfa2da336e48e6970bf2b78ba40268bd40e3
z <z@z>
2020-07-07 00:46:05 +0800

    fix source multi, zsh init multi

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/zsh
+# get debug flag
+_owen_debug=$1
+echo -n $1
+
 # cover local config
 cp -r etc ~/.local/
+
 # enable config file and avoid source twice
-if [[ $owen_inited = 1 ]] {
-} else {
+if [[ -z $_owen_inited ]] {
+	if [[ -n $_owen_debug ]] {
+		echo _owen_inited=$_owen_inited
+	}
 	echo "source ~/.local/etc/zsh.conf">>~/.zshrc
 	echo "source ~/.local/etc/tmux.conf">>~/.tmux.conf
+} else {
+	if [[ -n $_owen_debug ]] {
+		echo "already sourced"
+	}
 }
