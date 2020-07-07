7c0575aefc6d93f605bcc837ce8809e09ffdfb89
z <z>
2020-07-07 20:44:58 +0800

    add double secure to avoid multi echo and source. If multi echo, only one source

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/zsh
-# get debug flag
-_owen_debug=$1
-echo -n $1
-
 # cover local config
 cp -r etc ~/.local/
 
-# enable config file and avoid source twice
-if [[ -z $_owen_inited ]] {
-	if [[ -n $_owen_debug ]] {
-		echo _owen_inited=$_owen_inited
-	}
+# -- zsh ----------------------------------------------------------
+# To activate the new .zshrc because this exists in father zsh
+unset _owen_zsh_sourced
+
+# enable config file and avoid echoed twice
+if [[ -z $_owen_zsh_echoed ]] {
+  echo "# -- owen zsh config -------------------------">>~/.zshrc
+	echo "unset _owen_zsh_sourced">>~/.zshrc    # To activate config bacause of the father config
 	echo "source ~/.local/etc/zsh.conf">>~/.zshrc
+} else {
+  echo "owen zsh echoed"
+}
+
+# -- tmux ---------------------------------------------------------
+# To activate the new .zshrc because this exists in father zsh
+unset _owen_tmux_sourced
+
+# enable config file and avoid echoed twice
+if [[ -z $_owen_tmux_echoed ]] {
+  echo "# -- owen tmux config -------------------------">>~/.tmux.conf
+	echo "unset _owen_tmux_sourced">>~/.tmux.conf     # To activate config bacause of the father config
 	echo "source ~/.local/etc/tmux.conf">>~/.tmux.conf
 } else {
-	if [[ -n $_owen_debug ]] {
-		echo "already sourced"
-	}
+  echo "owen tmux echoed"
 }
