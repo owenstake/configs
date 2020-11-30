a8f7db41560e8162160ea757aaaeeaded03a45e3
z <z@y500wsl2>
2020-12-01 01:17:38 +0800

    automate win10 keyremap with wsl

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,8 +32,7 @@ if [ $result -eq 1 ]
 then
     echo "we are in wsl~~~"
     cp ~/.local/etc/pac.txt /mnt/c/MY_SOFTWARE/v2rayN-windows-64/v2rayN-Core-64bit/pac.txt
-    cp ~/.local/etc/win10/keyremap.ahk /mnt/d/keyremap.ahk
-    explorer.exe "D:\keyremap.ahk"
+    rsync -r ~/.local/etc/win10/* /mnt/d/.local/
 fi
 
 # -- zsh ----------------------------------------------------------
