7539ac3e136c3630d3d2391b2bad10fe98aec76b
z <z@y500wsl2>
2020-12-25 01:24:17 +0800

    tmp

--- etc/win10/file2clip.ps1
+++ etc/win10/file2clip.ps1
@@ -1,10 +1,13 @@
 # copy file to clipboard for win10 explorer
 
-Param([Parameter(Mandatory = $True, Position = 1)][string] $filePath)
+# Param([Parameter(Mandatory = $True, Position = 1)][string] $filePath)
 Add-Type -AssemblyName System.Windows.Forms
 $files = new-object System.Collections.Specialized.StringCollection
+
+$args[0]
+$args[1]
 
 # FullName is need. Get-Item usage can refer to 
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-item?view=powershell-7.1
 $files.Add((Get-Item -Path $filePath).FullName)
-[System.Windows.Forms.Clipboard]::SetFileDropList($files)
+# [System.Windows.Forms.Clipboard]::SetFileDropList($files)
