87567a4438d1650fabd9fd061ed7b46cd012050d
z <z@y500wsl2>
2020-12-25 01:12:34 +0800

    pw yw for multi files

--- etc/win10/clip2file.ps1
+++ etc/win10/clip2file.ps1
@@ -1,16 +1,23 @@
+# copy file from clipboard to DesDir for win10 explorer
 
+Param([Parameter(Mandatory = $True, Position = 1)][string] $DesDirPath)
 Add-Type -AssemblyName System.Windows.Forms
+$fileDropList = new-object System.Collections.Specialized.StringCollection;
 
 if (-not [System.Windows.Forms.Clipboard]::ContainsFileDropList()) {
     Write-Verbose "No appendable content was found."
 } else {
-    [System.Collections.Generic.List[string]]$fileDropList = [System.Windows.Forms.Clipboard]::GetFileDropList()
+    $fileDropList = [System.Windows.Forms.Clipboard]::GetFileDropList()
 }
 
-foreach ($item in $fileDropList) {
- [console]::writeline($item)
- [console]::writeline("\n")
+foreach ($file in $fileDropList) {
+    Copy-Item -Path $file -Destination (Get-Item -Path $DesDirPath).FullName -Recurse
 }
-[console]::writeline("hello world")
 
-# ($fileDropList).ForEach([console]::writeline)
+# foreach ($bird in $birds)
+# {
+#   $count += 1
+#   "$bird = " + $bird.length
+# Write-Host
+# }
+# "Total number of birds is $count."
