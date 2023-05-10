f2f2e28c94f0b62b53fe329163e2472640c3655b
mlshenhua <mlshenhua@gmail.com>
2023-05-10 18:30:32 +0800

    update

--- etc/ranger/plugins/ranger-fzf.py
+++ etc/ranger/plugins/ranger-fzf.py
@@ -25,22 +25,26 @@ class fzf(ranger.Command):
             -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
 
         env = os.environ.copy()
-        env['FZF_DEFAULT_COMMAND'] = "fzf-tmux"
-        env['FZF_DEFAULT_OPTS'] = '--height=100% --layout=reverse --ansi        \
-            --prompt "All> "                                                    \
-            --header "CTRL-D: Directories / CTRL-F: Files"                      \
-            --bind "ctrl-d:change-prompt(Directories> )+reload(find * -type d)" \
-            --bind "ctrl-f:change-prompt(Files> )+reload(find * -type f)"       \
-            --preview="{}"'.format('''
-               (
-                   ~/.config/fzf/fzf-scope.sh {} ||
-                   #batcat --color=always {} ||
-                   #bat --color=always {} ||
-                   #cat {} ||
-                   tree -ahpCL 3 -I '.git' -I '*.py[co]' -I '__pycache__' {}
-               ) 2>/dev/null | head -n 100
-            ''')
+        env['FZF_DEFAULT_COMMAND'] = "fzf"
+        # env['FZF_DEFAULT_COMMAND'] = "fzf-tmux"
 
+        #env['FZF_DEFAULT_OPTS'] = '--height=100% --layout=reverse --ansi        \
+        #    --prompt "All> "                                                    \
+        #    --header "CTRL-D: Directories / CTRL-F: Files"                      \
+        #    --bind "ctrl-d:change-prompt(Directories> )+reload(find * -type d)" \
+        #    --bind "ctrl-f:change-prompt(Files> )+reload(find * -type f)"       \
+        #    --bind                                                           \
+        #    --preview="{}"'.format('''
+        #       (
+        #           ~/.config/fzf/fzf-scope.sh {} ||
+        #           #batcat --color=always {} ||
+        #           #bat --color=always {} ||
+        #           #cat {} ||
+        #           tree -ahpCL 3 -I '.git' -I '*.py[co]' -I '__pycache__' {}
+        #       ) 2>/dev/null | head -n 100
+        #    ''')
+
+        env['FZF_DEFAULT_OPTS'] = env['FZF_CTRL_T_OPTS'] + '--reverse --height 50%'
 
         fzf = self.fm.execute_command(command, env=env, stdout=subprocess.PIPE)
         stdout, _ = fzf.communicate()
