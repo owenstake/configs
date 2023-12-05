d0072513ee696c590ea0b758cabcaba54b140656
owen <owen@fs3b2-lVyLGF3d1>
2023-12-05 11:06:26 +0800

    update tab and keybind for no-wsl

--- ubt/make-ubt.sh
+++ ubt/make-ubt.sh
@@ -3,145 +3,148 @@ source lib.sh
 ScriptDir=$(realpath $(dirname $0))
 
 AddHookToConfigFile() {
-    local file="$1"
-    local msg="$2"
-    local commentMarker=${3:-"#"}
-    local MarkLine="$commentMarker owen configed" 
-    local line="${msg} ${MarkLine}"
+	local file="$1"
+	local msg="$2"
+	local commentMarker=${3:-"#"}
+	local MarkLine="$commentMarker owen configed"
+	local line="${msg} ${MarkLine}"
 
-    # touch file and mkdir -p
-    if [ ! -e "$file" ]; then
-        mkdir -p $(dirname $file)
-        touch $file
-    fi
+	# touch file and mkdir -p
+	if [ ! -e "$file" ]; then
+		mkdir -p $(dirname $file)
+		touch $file
+	fi
 
-    # check if config item is already in config file
-    local itemNum=$(grep -c -F "$MarkLine" $file)
-    case $itemNum in
-	    0)
-            fmt_info "Add config item to $file"
-            echo "$line" >> $file
-            ;;
-	    1)
-            fmt_info "Update config item in $file"
-            lineNum=$(grep -n "$MarkLine" $file | cut -d':' -f1)
-            sed -i "$lineNum c $line" $file
-            ;;
-	    *)
-            fmt_error "Fail to update owen config in $file"
-            fmt_error "There are more then one line exists in $file"
-        ;;
-    esac
+	# check if config item is already in config file
+	local itemNum=$(grep -c -F "$MarkLine" $file)
+	case $itemNum in
+		0)
+			fmt_info "Add config item to $file"
+			echo "$line" >> $file
+			;;
+		1)
+			fmt_info "Update config item in $file"
+			lineNum=$(grep -n "$MarkLine" $file | cut -d':' -f1)
+			sed -i "$lineNum c $line" $file
+			;;
+		*)
+			fmt_error "Fail to update owen config in $file"
+			fmt_error "There are more then one line exists in $file"
+		;;
+	esac
 }
 
 DeployConfigDir() {
-    local srcDir=$1
-    local dstDir=$2
-    mkdir -p $dstDir
-    rsync -r $srcDir/* $dstDir
-    fmt_info "DeployConfigDir $srcDir to $dstDir"
+	local srcDir=$1
+	local dstDir=$2
+	mkdir -p $dstDir
+	rsync -r $srcDir/* $dstDir
+	fmt_info "DeployConfigDir $srcDir to $dstDir"
 }
 
 DeployConfigFile() {
-    local srcFile=$1
-    local dstFile=$2
-    fmt_info "DeployConfigFile $srcFile to $dstFile"
-    mkdir -p $(dirname $dstFile)
-    rsync $srcFile $dstFile
+	local srcFile=$1
+	local dstFile=$2
+	fmt_info "DeployConfigFile $srcFile to $dstFile"
+	mkdir -p $(dirname $dstFile)
+	rsync $srcFile $dstFile
 }
 
 MakeInstall() {
-    ## override config
-    # mkdir -p ~/.local $$ mkdir -p ~/.config
-    DeployConfigDir   etc/ranger     $HOME/.config/ranger/
-    DeployConfigDir   etc/fzf        $HOME/.config/fzf/
+	## override config
+	# mkdir -p ~/.local $$ mkdir -p ~/.config
+	DeployConfigDir   etc/ranger     $HOME/.config/ranger/
+	DeployConfigDir   etc/fzf        $HOME/.config/fzf/
 
-    # local config
-    # DeployConfigDir   ../common/etc/vim ~/.config/vim
-    DeployConfigDir   ../common/etc/vim   $OwenInstallDir/etc/vim/
-    DeployConfigDir   etc/tmux            $OwenInstallDir/etc/tmux/
-    DeployConfigDir   etc/zsh             $OwenInstallDir/etc/zsh/
-    DeployConfigDir   etc/newsboat        $OwenInstallDir/etc/newsboat/
+	# local config
+	# DeployConfigDir   ../common/etc/vim ~/.config/vim
+	DeployConfigDir   ../common/etc/vim   $OwenInstallDir/etc/vim/
+	DeployConfigDir   etc/tmux            $OwenInstallDir/etc/tmux/
+	DeployConfigDir   etc/zsh             $OwenInstallDir/etc/zsh/
+	DeployConfigDir   etc/newsboat        $OwenInstallDir/etc/newsboat/
 
-    DeployConfigDir   etc/keymap        $OwenInstallDir/etc/keymap/
-    DeployConfigDir   bin               $OwenInstallDir/bin
+	DeployConfigDir   etc/keymap        $OwenInstallDir/etc/keymap/
+	DeployConfigDir   bin               $OwenInstallDir/bin
 
-    if [ -f ../common/etc/init-in-one.lua ] ; then
-        DeployConfigFile ../common/etc/init-in-one.lua \
-                            ~/.config/nvim/init.lua
-    fi
+	if [ -f ../common/etc/init-in-one.lua ] ; then
+		DeployConfigFile ../common/etc/init-in-one.lua \
+							~/.config/nvim/init.lua
+	fi
 
-    # echo "export PATH=$PATH:$OwenInstallDir/bin" >> ~/.profile
+	# echo "export PATH=$PATH:$OwenInstallDir/bin" >> ~/.profile
 
-    # xbindkeys config
-    if command_exists xbindkeys; then
-	DeployConfigFile etc/keymap/xbindkeysrc ~/.xbindkeysrc
-    fi
+	# xbindkeys config
+	if ! InWsl ; then
+		# keybind only for no-wsl. wsl already has win ahk for keybind.
+		if command_exists xbindkeys; then
+			DeployConfigFile etc/keymap/xbindkeysrc ~/.xbindkeysrc
+		fi
 
-    # xmodmap config
-    if command_exists xmodmap; then
-	DeployConfigFile etc/keymap/xmodmap ~/.Xmodmap
-    fi
+		# xmodmap config
+		if command_exists xmodmap; then
+			DeployConfigFile etc/keymap/xmodmap ~/.Xmodmap
+		fi
+	fi
 
-    # Generate ssh config file
-    if [[ $(uname -a) == *WSL* ]] ; then
-        fmt_info "Generate ssh config and install"
-        jsonConfig=$(find .. -name "proxy.json" -exec realpath {} \;)
-        if [[ -z $jsonConfig ]] ; then
-            fmt_error "No found file proxy.json"
-        else
-            SearchAndExecFile \
-                ".." \
-                "sshconfig.py" \
-                $jsonConfig \
-                $OwenInstallDir/etc/ssh/config
-        fi
-    fi
+	# Generate ssh config file
+	if InWsl ; then
+		fmt_info "Generate ssh config and install"
+		jsonConfig=$(find .. -name "proxy.json" -exec realpath {} \;)
+		if [[ -z $jsonConfig ]] ; then
+			fmt_error "No found file proxy.json"
+		else
+			SearchAndExecFile \
+				".." \
+				"sshconfig.py" \
+				$jsonConfig \
+				$OwenInstallDir/etc/ssh/config
+		fi
+	fi
 
-    fmt_info "-- Deploy hooks to config file ---------"
-    AddHookToConfigFile   \
-	    ~/.vimrc      \
-	    "source $OwenInstallDir/etc/vim/vimrc"   '"'
-    AddHookToConfigFile   \
-	    ~/.zshrc      \
-	    "source $OwenInstallDir/etc/zsh/zshrc"
-    AddHookToConfigFile   \
-	    ~/.tmux.conf  \
-	    "source $OwenInstallDir/etc/tmux/tmux.conf"
-    AddHookToConfigFile   \
-	    ~/.ssh/config \
-	    "Include $OwenInstallDir/etc/ssh/config"
-    AddHookToConfigFile   \
-	    ~/.profile    \
-	    "[ -d $OwenInstallDir/bin ] && PATH=\"$OwenInstallDir/bin:\$PATH\""
+	fmt_info "-- Deploy hooks to config file ---------"
+	AddHookToConfigFile   \
+		~/.vimrc      \
+		"source $OwenInstallDir/etc/vim/vimrc"   '"'
+	AddHookToConfigFile   \
+		~/.zshrc      \
+		"source $OwenInstallDir/etc/zsh/zshrc"
+	AddHookToConfigFile   \
+		~/.tmux.conf  \
+		"source $OwenInstallDir/etc/tmux/tmux.conf"
+	AddHookToConfigFile   \
+		~/.ssh/config \
+		"Include $OwenInstallDir/etc/ssh/config"
+	AddHookToConfigFile   \
+		~/.profile    \
+		"[ -d $OwenInstallDir/bin ] && PATH=\"$OwenInstallDir/bin:\$PATH\""
 
-    # WSL config.
-    if [[ $(uname -a) == *WSL* ]]; then
-        fmt_info "We are in wsl~~~"
-        # powershell.exe -c "../win/make-win.ps1 install"
-    fi
+	# WSL config.
+	if [[ $(uname -a) == *WSL* ]]; then
+		fmt_info "We are in wsl~~~"
+		# powershell.exe -c "../win/make-win.ps1 install"
+	fi
 
 }
 
 main() {
-    action=${1:-"install"}
-    case $action in  
-        install)  
-            fmt_info "Installing ~~"  
-            MakeInstall
-            ;;  
-        uninstall)  
-            fmt_info "Uninstalling ~~"  
-            ;;  
-        clean)  
-            fmt_info "Cleaning ~~"
-            ;;
-        all)  
-            fmt_info "Cleaning ~~"
-            ;;
-        *)
-            fmt_error "unknown action $1"
-    esac
+	action=${1:-"install"}
+	case $action in
+		install)
+			fmt_info "Installing ~~"
+			MakeInstall
+			;;
+		uninstall)
+			fmt_info "Uninstalling ~~"
+			;;
+		clean)
+			fmt_info "Cleaning ~~"
+			;;
+		all)
+			fmt_info "Cleaning ~~"
+			;;
+		*)
+			fmt_error "unknown action $1"
+	esac
 }
 
 main $@
