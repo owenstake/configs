178040be5d08039e15fa0c39a13a3383e883516b
owen <owen@fs3b2-lVyLGF3d1>
2023-12-22 19:46:36 +0800

    add golang/rust binstall

--- cli-mini/setup-sys/make.sh
+++ cli-mini/setup-sys/make.sh
@@ -56,26 +56,69 @@ bashrc_for_tmux="
 
 vim_config="inoremap jk <esc>"
 
-git_repo_build() {
-    local bin_name=$1
-    local repo_name=$2
-    local cpu_arch=$(uname -m)
-    # match .tar.gz .tar.xa .tgz
-    local file_pattern=${3:-"${cpu_arch}-unknown-linux-musl\.t.*[xg]z$"}
-    if [ -n "$3" ] ; then
-        file_pattern=${file_pattern/x86_64/$cpu_arch}
-        if [ "$cpu_arch" = "aarch64" ] ; then
-            file_pattern=${file_pattern/amd64/arm64}
+try_get_download_url() {
+    local type=$1
+    local release_info=$2
+    local ext_pattern="(t.*[xg]z|zip)"  # match .tar.gz .tar.xa .tgz
+    case $type in
+        rust)
+            local cpu_arch=$(uname -m)
+            # TODO: need support linux-gnu ? Too complicated.
+            local basename_pattern="${cpu_arch}-unknown-linux-musl"
+            ;;
+        golang)
+            local cpu_arch=$(uname -m | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
+            local basename_pattern="linux[-_]${cpu_arch}"
+            ;;
+        *)
+            fmt_error "unknown type $type"
+            return -1
+    esac
+    local file_pattern="${basename_pattern}.${ext_pattern}$"
+    local dl_url=$(echo "$release_info" | jq -r '.assets[] | select(.name |
+                        match("'$file_pattern'")).browser_download_url'  )
+    if [ $? -ne 0 ] || [ -z "$dl_url" ]; then
+        fmt_error "Get dl_url fail, file_pattern match nothing"
+        return -1
+    else
+        echo "$dl_url"  # output
+    fi
+    return
+}
+
+base_binstall() {
+    local language=$1
+    local bin_name=$2
+    local repo_name=$3
+    fmt_info "Download $bin_name in $file_pattern from $repo_name"
+    local GITHUB_API_REMAIN_COUNTS=$(curl -sf https://api.github.com/rate_limit \
+                                                | jq '.rate.remaining')
+    if [ $GITHUB_API_REMAIN_COUNTS -lt 30 ]; then
+        fmt_info "Github API count has be exhausted, use github token."
+        if [ -z $GITHUB_TOKEN ]; then
+            fmt_error "Please set GITHUB_TOKEN into env"
+            return -1
+        else
+            local CURL_OPTION="--header \"Authorization: Bearer $GITHUB_TOKEN\""
         fi
     fi
-    # local file_pattern_by=${3:-"x86_64-unknown-linux-musl.*tar.[xg]z$"}
-    fmt_info "Download $bin_name in $file_pattern from $repo_name"
-
-    local dl_url=$(curl -s https://api.github.com/repos/$repo_name/releases/latest |
-                jq -r '.assets[] | select(.name | match("'$file_pattern'"))
-                        .browser_download_url'  )
-    if [ -z "$dl_url" ] ; then
-        fmt_error "dl_url is empty $dl_url fail"
+    local release_info=$(curl -sf $CURL_OPTION https://api.github.com/repos/$repo_name/releases/latest)
+    if [ $? -ne 0 ] ; then
+        fmt_error "Curl github api fail"
+        return -1
+    fi
+    # if [ -z "$release_info" ] ; then
+    #     fmt_error "curl github api ok, but release_info is null"
+    #     return -1
+    # fi
+    # if echo "$release_info" | grep "API rate limit" ; then
+    #     fmt_error "github API rate limit!\n$release_info"
+    #     return -1
+    # fi
+    fmt_error "$release_info" 2>&1 | head -n5
+    local dl_url=$(try_get_download_url "$language" "$release_info" "$file_pattern")
+    if [ $? -ne 0 ] ; then
+        fmt_error "dl_url get fail $dl_url "
         return -1
     fi
 
@@ -108,8 +151,13 @@ git_repo_build() {
             ;;
         *.zip)
             # unzip can not read from stdin
-            echo " curl -sL $dl_url | unzip - -Wjo "**$bin_name" -d $dst_dir"
-            curl -sL $dl_url | unzip - -Wo "**$bin_name" -d $dst_dir -j
+            # echo " curl -sL $dl_url | unzip - -Wjo "**$bin_name" -d $dst_dir"
+            # curl -sL $dl_url | unzip - -Wo "**$bin_name" -d $dst_dir -j
+            echo "zip url dl_url"
+            tmpfile="/tmp/temp.zip"
+            curl -L $dl_url -o $tmpfile && 
+                    unzip -j $tmpfile "**/$bin_name" -d $dst_dir &&
+                    rm $tmpfile
             ;;
         *)
             fmt_error "unknow ext decompress for $dl_file"
@@ -117,37 +165,79 @@ git_repo_build() {
     esac
 }
 
+cargo_binstall() {
+    base_binstall "rust" "${@}"
+    return $?
+}
+
+golang_binstall() {
+    base_binstall "golang" "${@}"
+    return $?
+}
+
+golang_tools_install_by_direct_download() {
+    golang_binstall "lf"              "gokcehan/lf"
+    golang_binstall "cloudpan189-go"  "tickstep/cloudpan189-go"
+}
+
+rust_tools_install_by_cargo_binstall() {
+    cargo_binstall "cargo-binstall"  "cargo-bins/cargo-binstall"
+    local cpu_arch=$(uname -m)
+    $buildDir/bin/cargo-binstall -y --no-discover-github-token  \
+                    --disable-strategies compile                \
+                    --targets "${cpu_arch}-unknown-linux-musl"  \
+                    --targets "${cpu_arch}-unknown-linux-gnu"   \
+                    --install-path $buildDir/bin                \
+                    bat fd-find ripgrep zoxide eza              \
+                    bandwhich bottom difftastic du-dust fselect \
+                    hyperfine lsd mcfly sd starship             \
+                    tokei watchexec-cli
+    return
+}
+
 # this func only for x86, unable to compat for multi os
-rust_tools_download() {
-    git_repo_build "cargo-binstall"  "cargo-bins/cargo-binstall"
-    # git_repo_build "cargo-binstall"  "cargo-bins/cargo-binstall"
-    # lf  is golang tool
+rust_tools_install_by_direct_download() {
+    # cargo_binstall "cargo-binstall"  "cargo-bins/cargo-binstall"
+
     # $buildDir/bin/cargo-binstall -y --install-path \
     #                 $buildDir/bin fd rg zoxide eza \
     #                 bat bandwhich btm delta difftastic dust fselect \
     #                 grex hyperfine lsd mcfly sd starship tokei watchexec
 
-    git_repo_build "fd"        "sharkdp/fd"
-    git_repo_build "rg"        "BurntSushi/ripgrep"
-    git_repo_build "zoxide"    "ajeetdsouza/zoxide"
-    git_repo_build "lf"        "gokcehan/lf"         "lf-linux-amd64.tar.gz$"
-    git_repo_build "eza"       "eza-community/eza"
-    git_repo_build "bat"       "sharkdp/bat"
+    # rust install for compile
+    # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    # lf is golang tool
+    # delta do no support cargo-binstall
+    # grex is not support in aarch64
 
-    git_repo_build "bandwhich" "imsnif/bandwhich"
-    git_repo_build "btm"       "ClementTsang/bottom"
-    git_repo_build "delta"     "dandavison/delta"
-    git_repo_build "difft"     "Wilfred/difftastic"  "difft-x86_64-unknown-linux-gnu.tar.gz"
-    git_repo_build "dust"      "bootandy/dust"
-    git_repo_build "fselect"   "jhspetersson/fselect" "fselect-x86_64-linux-musl.gz$"
-    git_repo_build "grex"      "pemistahl/grex"
-    git_repo_build "hyperfine" "sharkdp/hyperfine"
-    git_repo_build "lsd"       "lsd-rs/lsd"
-    git_repo_build "mcfly"     "cantino/mcfly"
-    git_repo_build "sd"        "chmln/sd"
-    git_repo_build "starship"  "starship/starship"
-    git_repo_build "tokei"     "XAMPPRocky/tokei"
-    git_repo_build "watchexec" "watchexec/watchexec"
+    # golang - direct download
+    cargo_binstall "lf"        "gokcehan/lf"         "lf-linux-amd64.tar.gz$"
+
+    # rust - direct download
+    cargo_binstall "delta"     "dandavison/delta"
+    cargo_binstall "difft"     "Wilfred/difftastic"  "difft-x86_64-unknown-linux-gnu.tar.gz"
+    cargo_binstall "grex"      "pemistahl/grex"
+
+    # rust - cargo-binstall
+    cargo_binstall "fd"        "sharkdp/fd"
+    cargo_binstall "rg"        "BurntSushi/ripgrep"
+    cargo_binstall "eza"       "eza-community/eza"
+    cargo_binstall "bat"       "sharkdp/bat"
+    cargo_binstall "watchexec" "watchexec/watchexec"
+    cargo_binstall "zoxide"    "ajeetdsouza/zoxide"
+
+    cargo_binstall "bandwhich" "imsnif/bandwhich"
+    cargo_binstall "btm"       "ClementTsang/bottom"
+    cargo_binstall "dust"      "bootandy/dust"
+    cargo_binstall "fselect"   "jhspetersson/fselect" "fselect-x86_64-linux-musl.gz$"
+    cargo_binstall "hyperfine" "sharkdp/hyperfine"
+
+    cargo_binstall "lsd"       "lsd-rs/lsd"
+    cargo_binstall "mcfly"     "cantino/mcfly"
+    cargo_binstall "sd"        "chmln/sd"
+    cargo_binstall "starship"  "starship/starship"
+    cargo_binstall "tokei"     "XAMPPRocky/tokei"
+
 
     return
 }
@@ -257,7 +347,8 @@ MakeAll() {
         # fzf. build bin file need github.
         zlua_config download
         fzf_config  download
-        rust_tools_download
+        golang_tools_install_by_direct_download
+        rust_tools_install_by_cargo_binstall
     fi
 }
 
